283cedf9e2f8dfac0690f5381d9923c1c00a658b
unknown <nicollysenai29@gmail.com>
2023-04-06 16:19:24 -0300

    foi feito um repositorio, logo após abrimos o vscode e fizemos um lista em python utilizamos tambem o codigo for e o print

---
+++ exercicios3.py
@@ -0,0 +1,8 @@
+notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+media = 0.0
+alunos = ['Nicolly', 'Mariana', 'Eloah', 'Nicole']
+print (notas[0])
+
+for valorindex in notas:
+    for nome in alunos:
+        print(nome+ ' ' + str(valorindex))
